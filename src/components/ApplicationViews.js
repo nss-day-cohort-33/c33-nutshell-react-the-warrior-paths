0604a740d349841abbe09811980ed7ec178dbc64
Karla Gallegos <kgalleg618@gmail.com>
2019-07-23 10:41:14 -0500

    working on welcome page

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,6 +1,8 @@
 import { Route, Redirect } from "react-router-dom";
 import React, { Component } from "react";
-
+import Welcome from "./authentication/Welcome"
+import Login from "./authentication/Login"
+import Register from "./authentication/Register"
 export default class ApplicationViews extends Component {
 
   render() {
@@ -9,7 +11,19 @@ export default class ApplicationViews extends Component {
 
         <Route
           exact path="/" render={props => {
-            return null
+            return <Welcome/>
+            // Remove null and return the component which will show news articles
+          }}
+        />
+        <Route
+           path="/login" render={props => {
+            return <Login/>
+            // Remove null and return the component which will show news articles
+          }}
+        />
+        <Route
+           path="/register" render={props => {
+            return <Register/>
             // Remove null and return the component which will show news articles
           }}
         />
@@ -34,7 +48,7 @@ export default class ApplicationViews extends Component {
             // Remove null and return the component which will show the user's tasks
           }}
         />
-        
+
       </React.Fragment>
     );
   }
--- src/components/authentication/Register.js
+++ src/components/authentication/Register.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react'
 
-export default class Login extends Component {
+export default class Register extends Component {
     render() {
         return (
+
             <div>
              <label>Username</label>
              <input type="text"></input>
+             <label>Email</label>
+             <input type="text"></input>
              <label>Password</label>
              <input type="password"></input>
-             <button>login</button>
+             <button>register</button>
             </div>
+
         )
     }
 }
